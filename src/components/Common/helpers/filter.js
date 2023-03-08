ad5686519b77e81236338c170d0f43eaecbb8108
BurtseffIlya <BurtseffIlya@yandex.ru>
2023-03-08 08:40:51 +0300

    clear

--- src/components/Common/helpers/filter.js
+++
@@ -1,7 +0,0 @@
-// filterList() {
-//    let filt = this.filterString
-//    return this.tasks.filter((item) => {
-//       if( filt === '') return true
-//       else return item.title.includes(this.filterString) || item.description.includes(this.filterString)
-//    })
-// }
